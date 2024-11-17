ab28e800caa2f74db01b23a4091e9cab07b55e88
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2024-11-17 10:17:45 -0300

    Paginated Reading

--- src/main/java/com/devsuperior/user_request_sb/step/fetchUserDataAndStoreDBStepConfig.java
+++ src/main/java/com/devsuperior/user_request_sb/step/fetchUserDataAndStoreDBStepConfig.java
@@ -1,2 +1,35 @@
-package com.devsuperior.user_request_sb.step;public class fetchUserDataAndStoreDBStepConfig {
+package com.devsuperior.user_request_sb.step;
+
+import com.devsuperior.user_request_sb.dto.UserDTO;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class fetchUserDataAndStoreDBStepConfig {
+
+    private final PlatformTransactionManager transactionManager;
+
+    @Value("${chunkSize}")
+    private int chunkSize;
+
+    public fetchUserDataAndStoreDBStepConfig(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public Step fetchUserDataAndStoreDBStep(ItemReader<UserDTO> fetchUserDataReader, JobRepository jobRepository, DataSourceTransactionManager transactionManager) {
+        return new StepBuilder("fetchUserDataAndStoreDBStep", jobRepository)
+                .<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+                .reader(fetchUserDataReader)
+                .build();
+    }
+
 }
