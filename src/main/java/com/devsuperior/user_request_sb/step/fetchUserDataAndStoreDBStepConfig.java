ac42baf99906255201f6f1b711330bac925b229e
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2024-11-17 16:35:42 -0300

    Configure multiple DBs

--- src/main/java/com/devsuperior/user_request_sb/step/fetchUserDataAndStoreDBStepConfig.java
+++ src/main/java/com/devsuperior/user_request_sb/step/fetchUserDataAndStoreDBStepConfig.java
@@ -1,12 +1,15 @@
 package com.devsuperior.user_request_sb.step;
 
 import com.devsuperior.user_request_sb.dto.UserDTO;
+import com.devsuperior.user_request_sb.entities.User;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,20 +19,27 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class fetchUserDataAndStoreDBStepConfig {
 
-    private final PlatformTransactionManager transactionManager;
+    @Autowired
+    @Qualifier("transactionManagerApp")
+    private PlatformTransactionManager transactionManager;
 
     @Value("${chunkSize}")
     private int chunkSize;
 
-    public fetchUserDataAndStoreDBStepConfig(PlatformTransactionManager transactionManager) {
-        this.transactionManager = transactionManager;
-    }
+//    public fetchUserDataAndStoreDBStepConfig(PlatformTransactionManager transactionManager) {
+//        this.transactionManager = transactionManager;
+//    }
 
     @Bean
-    public Step fetchUserDataAndStoreDBStep(ItemReader<UserDTO> fetchUserDataReader, ItemWriter<UserDTO> insertUserDataDBWriter, JobRepository jobRepository, DataSourceTransactionManager transactionManager) {
+    public Step fetchUserDataAndStoreDBStep(ItemReader<UserDTO> fetchUserDataReader,
+                                            ItemProcessor<UserDTO, User> selectFieldsUserDataProcessor,
+                                            ItemWriter<User> insertUserDataDBWriter,
+                                            JobRepository jobRepository,
+                                            DataSourceTransactionManager transactionManager) {
         return new StepBuilder("fetchUserDataAndStoreDBStep", jobRepository)
-                .<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+                .<UserDTO, User>chunk(chunkSize, transactionManager)
                 .reader(fetchUserDataReader)
+                .processor(selectFieldsUserDataProcessor)
                 .writer(insertUserDataDBWriter)
                 .build();
     }
--- src/main/java/com/devsuperior/user_request_sb/writer/InsertUserDataDBWriterConfig.java
+++ src/main/java/com/devsuperior/user_request_sb/writer/InsertUserDataDBWriterConfig.java
@@ -1,6 +1,7 @@
 package com.devsuperior.user_request_sb.writer;
 
 import com.devsuperior.user_request_sb.dto.UserDTO;
+import com.devsuperior.user_request_sb.entities.User;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class InsertUserDataDBWriterConfig {
 
     @Bean
-    public ItemWriter<UserDTO> insertUserDataDBWriter() {
+    public ItemWriter<User> insertUserDataDBWriter() {
         return users -> users.forEach(System.out::println);
     }
 
